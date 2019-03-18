3e92e2eb1abc8c03ed1de8d9edc89f76f10b558d
shihuncl <1002968882@qq.com>
2019-03-18 23:59:27 +0800

    提交代码

---
+++ src/main/java/factory/ICourse.java
@@ -0,0 +1,9 @@
+package factory;
+
+/**
+ * Created by cl on 2019/3/18.
+ */
+public interface ICourse {
+
+      void  record();
+}
---
+++ src/main/java/factory/JavaCourse.java
@@ -0,0 +1,13 @@
+package factory;
+
+/**
+ * Created by cl on 2019/3/18.
+ */
+public class JavaCourse implements ICourse {
+
+
+    @Override
+    public void record() {
+        
+    }
+}
