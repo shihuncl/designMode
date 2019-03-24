2a37b36b030e7c07d8f95411f4e2f9a75f899fc9
shihuncl <1002968882@qq.com>
2019-03-24 14:06:00 +0800

    分支2.0test

--- src/main/java/factory/ICourse.java
+++ src/main/java/factory/ICourse.java
@@ -2,6 +2,8 @@ package factory;
 
 /**
  * Created by cl on 2019/3/18.
+ *
+ * 测试分支2.0
  */
 public interface ICourse {
 
--- src/main/java/factory/JavaCourse.java
+++ src/main/java/factory/JavaCourse.java
@@ -2,6 +2,7 @@ package factory;
 
 /**
  * Created by cl on 2019/3/18.
+ * 测试分支2.0
  */
 public class JavaCourse implements ICourse {
 
