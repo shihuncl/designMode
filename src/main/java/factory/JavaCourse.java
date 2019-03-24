9cf2a7f47d5c8b1ae48ed909ce4cee743ea5cde1
shihuncl <1002968882@qq.com>
2019-03-24 14:28:10 +0800

    分支代码合并

--- src/main/java/factory/JavaCourse.java
+++ src/main/java/factory/JavaCourse.java
@@ -2,7 +2,6 @@ package factory;
 
 /**
  * Created by cl on 2019/3/18.
- * 测试分支2.0
  */
 public class JavaCourse implements ICourse {
 
--- src/main/java/factory/SimpleFactoryTest.java
+++ src/main/java/factory/SimpleFactoryTest.java
@@ -2,8 +2,7 @@ package factory;
 
 /**
  * Created by cl on 2019/3/18.
- *
- * 测试分支切换
+ * 版本4.0
  */
 public class SimpleFactoryTest {
     public static void main(String[] args) {
